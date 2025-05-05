42a0f4dd412baa10b64f96e437e2230894fa9468
Angelica Allen <angelicaallen@MacBookPro.attlocal.net>
2025-05-05 12:12:26 -0500

    Full deployment-ready backend

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, enum: ['referee', 'admin'], default: 'referee' }
+  role: { type: String, enum: ['admin', 'referee'], default: 'referee' }
 });
 
 module.exports = mongoose.model('User', UserSchema);
