329836773f9f33f11d6b754c4f082cd714c343f7
Angelica Allen <angelicaallen@MacBookPro.attlocal.net>
2025-05-05 17:47:13 -0500

    Index.js

--- index.js
+++ index.js
@@ -7,14 +7,16 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://diamondofficials.com',
+  credentials: true
+}));
 app.use(express.json());
 
 // Routes
 const authRoutes = require('./routes/auth');
 const dashboardRoutes = require('./routes/dashboard');
 
-// Use Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/dashboard', dashboardRoutes);
 
